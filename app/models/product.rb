8511721f21b4eba73f4d8b58861e6b9067eb2d77
Jeremy Tarling <jeremy.tarling@gmail.com>
2012-09-18 21:15:17 +0100

    added validation and fixed failing tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :image_url, :price, :title
+  attr_accessible :description, :price, :title, :image_url
+  validates :title, :description, :image_url, presence: true
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|jpeg|png)$}i,
+    message: 'must be a URL for a .gif, .jpeg or .png image'
+  }
 end
